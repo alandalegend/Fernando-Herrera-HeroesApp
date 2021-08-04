c45c6a683b70ed5df03321752d4486ce04e2664c
Alan [daLegend] <alanrob7@gmail.com>
2021-08-03 20:01:05 -0500

    update con mat

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
